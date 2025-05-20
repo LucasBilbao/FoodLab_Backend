bba7ffb7c72095a4853fff0eb52ea09932b48d2b
LucasBilbao <lucasbm789@gmail.com>
2025-05-20 17:13:43 +0400

    feat: update origin

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONT_END_URL], // frontend URL
-    methods: ['GET'],
+    origin: [process.env.FRONT_END_URL || 'https://lucasbilbao.github.io'], // frontend URL
+    methods: ['GET', 'POST'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
